38b5333024f5edc7a4778658235f553d74702b2a
Hejociara <hejociarav@gmail.com>
2023-10-04 10:30:14 -0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 alert ('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
-prompt ('Escolha um número entre 1 e 30');
+let chute = prompt ('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto){
+    alert ('Muito bem! Você acertou!!!(29)')
+    console.log('Muito bem! Você acertou!!!(29)');
+}
